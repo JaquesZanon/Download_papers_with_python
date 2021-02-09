0e06c9c82d5ad158966aa4c45258078e37a93c8f
Jaques Everton Zanon <jaques.zanon@gmail.com>
2021-02-09 13:25:10 -0300

    Update 1.py

--- 1.py
+++ 1.py
@@ -2,31 +2,16 @@
 """
 Created on Thu Feb  4 15:32:40 2021
 
-@author: jaques
+@author: Jaques Zanon
 """
 
-
-
 !pip3 install -U scidownl
-
-scidownl -l
-
 from scidownl.scihub import *
-import numpy as np
-import pandas as pd
 
-DOI = "10.1016/j.marpolbul.2016.08.011"
+
+DOIs = [...]# here is the list of papers DOIs 
 out = 'paper'
-sci = SciHub(DOI, out).download(choose_scihub_url_index=3)
-sci
-DOIs = [...]
-out = 'paper'
-for i in range(0,1,1): 
-    SciHub(DOIS[i], out).download(choose_scihub_url_index=3)
-  
-for doi in DOIS: 
-    SciHub(doi, out).download(choose_scihub_url_index=3)
-    
+   
 for doi in DOIS:
     try:
         SciHub(doi, out).download(choose_scihub_url_index=3)
@@ -38,21 +23,3 @@ for doi in DOIS:
     else:
         sci = SciHub(doi, out).download(choose_scihub_url_index=3)
     
-    
-    
-    
-    
-    
-    
-    
-    
-    
-
-df.loc[:100,0]
-data = np.loadtxt('lista.csv',delimiter=';',skiprows=1)
-df = pd.read_csv('lista.csv', delimiter=';')
-len(DOIs)
-range(DOIS)
-DOIS[0]
-
-
